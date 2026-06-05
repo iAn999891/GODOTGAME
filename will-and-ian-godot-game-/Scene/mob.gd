f37e3ec686450b8db12be80f23a9f425fe3e5256
willxin123 <willxin123@gmail.com>
2026-06-05 16:42:51 -0700

    gi

--- will-and-ian-godot-game-/Scene/mob.gd
+++ will-and-ian-godot-game-/Scene/mob.gd
@@ -4,7 +4,7 @@ var health = 5
 
 @onready var player = get_node("/root/Game_scene/Player")
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 70
 	move_and_slide()
