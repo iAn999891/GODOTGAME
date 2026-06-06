fa2bd56e0bcd40c7199fd3120a01b321330ef9c6
willxin123 <willxin123@gmail.com>
2026-06-05 20:26:07 -0700

    fix

--- will-and-ian-godot-game-/Scripts/Player_movment.gd
+++ will-and-ian-godot-game-/Scripts/Player_movment.gd
@@ -33,6 +33,6 @@ func _physics_process(_delta):
 	var overlapping_mobs = %Hitbox.get_overlapping_bodies()
 	if overlapping_mobs.size() > 0:
 		health -= DAMAGE_RATE * overlapping_mobs.size() * _delta
-		%ProgressBar.value = 50
+		%ProgressBar.value = health
 		if health <= 0.0:
 			health_depleted.emit()
