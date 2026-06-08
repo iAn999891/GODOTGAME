d0f33bb37245595a59495d460b1e057040f68420
willxin123 <willxin123@gmail.com>
2026-06-08 14:21:24 -0700

    onion

--- will-and-ian-godot-game-/Scripts/game_scene.gd
+++ will-and-ian-godot-game-/Scripts/game_scene.gd
@@ -1,15 +1,22 @@
 extends Node2D
 
 
-func spawn_mob():
+func spawn_carrot():
 	var new_mob = preload("res://Scene/BabyCarrot.tscn").instantiate()
+	%PathFollow2D.progress_ratio = randf()
+	new_mob.global_position = %PathFollow2D.global_position
+	add_child(new_mob)
+
+func spawn_onion():
+	var new_mob = preload("res://Scene/BabyOnion.tscn").instantiate()
 	%PathFollow2D.progress_ratio = randf()
 	new_mob.global_position = %PathFollow2D.global_position
 	add_child(new_mob)
 
 
 func _on_timer_timeout() -> void:
-	spawn_mob()
+	spawn_onion()
+	spawn_carrot()
 
 
 func _on_player_health_depleted() -> void:
