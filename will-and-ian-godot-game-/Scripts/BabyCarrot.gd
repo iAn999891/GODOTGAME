4b886faa8cd360657a01135554069131a798601d
iAn999891 <ianhszhang@gmail.com>
2026-06-09 14:52:58 -0700

    sigma boy

--- will-and-ian-godot-game-/Scripts/BabyCarrot.gd
+++ will-and-ian-godot-game-/Scripts/BabyCarrot.gd
@@ -2,22 +2,37 @@ extends CharacterBody2D
 
 var health = 3
 
-@onready var player = get_node("/root/Game_scene/Player")
-
 @export var damage = 15
-	
+@export var move_speed = 50
+
+@onready var player = get_node("/root/Game_scene/Player")
+@onready var nav_agent = $NavigationAgent2D
+
 func _physics_process(_delta):
-	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 50
-	
-	if player.global_position.x > global_position.x:
+
+	if player == null:
+		return
+
+	# Tell the agent where we want to go
+	nav_agent.target_position = player.global_position
+
+	# Get next point on the path
+	var next_pos = nav_agent.get_next_path_position()
+
+	# Move toward that point
+	var direction = global_position.direction_to(next_pos)
+	velocity = direction * move_speed
+
+	# Flip sprite
+	if velocity.x > 0:
 		$BabyCarrotSprite.flip_h = true
 	else:
 		$BabyCarrotSprite.flip_h = false
+
 	move_and_slide()
 
 func take_damage():
 	health -= 1
-			
-	if health == 0:
+
+	if health <= 0:
 		queue_free()
