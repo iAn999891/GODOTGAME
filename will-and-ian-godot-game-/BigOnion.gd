4db56aaec9c8b7469ad58edeb8435095a7e4d382
iAn999891 <ianhszhang@gmail.com>
2026-06-16 14:48:28 -0700

    hi

---
+++ will-and-ian-godot-game-/BigOnion.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+
+var health = 1
+
+@onready var player = get_node("/root/Game_scene/Player")
+
+@export var damage = 45
+
+func _physics_process(_delta):
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * 75
+	
+	if player.global_position.x > global_position.x:
+		$BabyOnionSprite.flip_h = true
+	else:
+		$BabyOnionSprite.flip_h = false
+	move_and_slide()
+
+func take_damage():
+	health -= 1
+			
+	if health == 0:
+		queue_free()
